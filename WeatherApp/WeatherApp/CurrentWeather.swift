944b9d2dcc1d89c932b76f7283d8ef89ae98e268
Olga Dakhel <olyapugovi4ka@gmail.com>
2019-09-29 15:14:48 +0300

    Successfully finished

---
+++ WeatherApp/WeatherApp/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by MacBook on 24/09/2019.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherApp/WeatherIconManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherIconManager.swift
+//  WeatherApp
+//
+//  Created by MacBook on 24/09/2019.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import Foundation
