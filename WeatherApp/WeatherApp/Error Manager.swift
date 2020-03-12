7d4f75343e1b9bcfa8b89323b9bb0fcabffdd0c9
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2020-03-12 13:17:32 +0300

    update

---
+++ WeatherApp/WeatherApp/Error Manager.swift
@@ -0,0 +1,14 @@
+//
+//  Error Manager.swift
+//  WeatherApp
+//
+//  Created by MacBook on 26/09/2019.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import Foundation
+
+public let ODNetworkingErrorDomain = "ru.OlgaDakhel.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
+public let UnexpectedResponseError = 200
+
--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperaureLabel: UILabel!
+    @IBOutlet weak var apparantTemperatureLabel: UILabel!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    let locationManager = CLLocationManager()
+    
+    lazy var weatherManager = APIWeatherManager(apiKey: "d101880b3e28fa88e134de7cd1cf9322")
+    let cordinates = Coordinates(latitude: 37.8267, longitude: 122.4233)
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        getCurrentWeatherData()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last! as CLLocation
+        print("my location latitude: \(userLocation.coordinate.latitude), longitude: \(userLocation.coordinate.longitude)")
+    }
+    
+    fileprivate func getCurrentWeatherData() {
+        weatherManager.fetchCurrentWeatherWith(coordinates: cordinates) { (result) in
+            self.toggleActivityIndicator(on: false)
+            switch result {
+            case .Success(let currentWeather):
+                self.updateUIWith(currentWeather: currentWeather)
+            case .Failure(let error as NSError):
+                self.vcError(title: "Unable to get data", message: "\(error.localizedDescription)", error: error)
+            }
+        }
+    }
+    
+    func vcError(title: String, message: String, error: NSError){
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        ac.addAction(ok)
+        present(ac,animated: true, completion:  nil)
+    }
+    func updateUIWith(currentWeather: CurrentWeather){
+        self.weatherImage.image = currentWeather.icon
+        self.pressureLabel.text = currentWeather.pressureString
+        self.humidityLabel.text = currentWeather.humidityString
+        self.temperaureLabel.text = currentWeather.temperatureString
+        self.apparantTemperatureLabel.text = currentWeather.appearantTemperatureString
+        
+    }
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+        toggleActivityIndicator(on: true)
+        getCurrentWeatherData()
+    }
+    
+    func toggleActivityIndicator(on: Bool){
+        refreshButton.isHidden = on
+        if on {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
 }
+
+
 
